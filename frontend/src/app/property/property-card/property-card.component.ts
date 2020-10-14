dabcafe771f6eca297a510188bd48255ef7fb8d4
wfd2018 <warmthlover@outlook.com>
2020-10-14 17:16:36 -0500

    commit all now

---
+++ frontend/src/app/property/property-card/property-card.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+selector: 'app-property-card',
+templateUrl: 'property-card.component.html',
+styleUrls: ['property-card.component.css']
+})
+
+export class PropertyCardComponent {
+Property: any = {
+'id': 1,
+'Name': 'Birla House',
+'Type':'House',
+'Price': 12000
+}
+
+}
