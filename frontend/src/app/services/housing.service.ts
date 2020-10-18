3aa2a7feab7b5edaf0dbeaeb17ffd97db6160456
wfd2018 <warmthlover@outlook.com>
2020-10-17 19:22:50 -0500

    var

--- frontend/src/app/services/housing.service.ts
+++ frontend/src/app/services/housing.service.ts
@@ -2,6 +2,7 @@ import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {map} from 'rxjs/operators';
 import {IProperty} from '../property/IProperty.interface';
+import {Observable} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
